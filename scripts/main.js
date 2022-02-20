f79048d5656ba6a1164f7b3567fa52c4a23b26d2
shengyu <shengyu7697@gmail.com>
2022-02-20 15:37:42 +0800

    add ga event

--- scripts/main.js
+++ scripts/main.js
@@ -45,16 +45,32 @@ let myButton3 = document.getElementById('btn3');
 
 myButton1.onclick = function() {
   console.log("Button1 click");
+  gtag('event', '按鈕點擊', {
+    'event_category': '一般',
+    'event_label': 'btn 1'
+  });
 }
 
 myButton2.onclick = function() {
   console.log("Button2 click");
+  gtag('event', '按鈕點擊', {
+    'event_category': '一般',
+    'event_label': 'btn 2'
+  });
 }
 
 myButton3.onclick = function() {
   console.log("Button3 click");
+  gtag('event', '按鈕點擊', {
+    'event_category': '一般',
+    'event_label': 'btn 3'
+  });
 }
 
 function button4OnClick() {
   console.log("Button4 click");
+  gtag('event', '按鈕點擊', {
+    'event_category': '一般',
+    'event_label': 'btn 4'
+  });
 }
